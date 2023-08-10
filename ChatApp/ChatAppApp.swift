3fb74062305fc840b42fb72fc7b373ebf855a440
Armando <jarodriguezvera@gmail.com>
2023-08-10 11:49:17 -0600

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Armando Rodríguez on 10/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
